9332f68d0143835963a13e0d250d3cbef0275611
jnjt37 <jnjt37@mst.edu>
2017-07-02 15:04:59 -0500

    Here we are

---
+++ Project 4/Project 4.cpp
@@ -0,0 +1,79 @@
+/*
+Name: Jaxson Johnston
+Assignment: Project 4
+*/
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "class_header.h"
+
+using namespace std;
+
+int main()
+{
+	Student *students;
+	int num_students = 0;
+	int enrollment_size = 5;
+	students = new Student[enrollment_size];
+	ifstream student_file;
+	student_file.open("students.txt");
+	while (!student_file.eof())
+	{
+		if (num_students == enrollment_size)
+		{
+			Student *q;
+			enrollment_size = enrollment_size * 2 + 5;
+			q = new Student[enrollment_size];
+			for (int j = 0; j < num_students; j ++)
+			{
+				q[j] = students[j];
+			}
+			if (students != nullptr)
+				delete[] students;
+			students = q;
+		}
+		cin >> students[num_students];
+		num_students++;
+	}
+	student_file.close();
+	ifstream schedules_file;
+	schedules_file.open("schedules.txt");
+	while (!schedules_file.eof())
+	{
+		int temp_ID;
+		int student_ID;
+		Course course_temp;
+		cin >> course_temp;
+		cin >> temp_ID;
+		for (int i = 0; i < num_students; i++)
+		{
+			student_ID = students[i].get_ID();
+			if (temp_ID == student_ID)
+			{
+				students[i].add_course(course_temp);
+			}
+		}
+	}
+	schedules_file.close();
+	ofstream student_schedules;
+	student_schedules.open("studentSchedules.txt");
+	for (int k = 0; k < enrollment_size; k++)
+	{
+		cout << students[k];
+		int temp_num_courses=0;
+		temp_num_courses = students[k].get_num_courses();
+		for (int l = 0; l < temp_num_courses; l++)
+		{
+			cout << students[k].p_courses[l]<<endl;
+		}
+		cout<<"$"<<fixed<<setprecision(2)<<students[k].tuition_due()<<endl;
+	}
+	student_schedules.close();
+	ofstream enrolled_students;
+	enrolled_students.open("courseEnrollments.txt");
+	
+	enrolled_students.close();
+    return 0;
+}
+
---
+++ Project 4/class_header.h
@@ -0,0 +1,127 @@
+#pragma once
+#ifndef CLASS_HEADER_H
+#define CLASS_HEADER_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+class Student
+{
+private:
+	string first_name, last_name;		
+	int ID,num_courses=0;
+	Course *p_courses;
+public:
+	Student(string first_name = "", string last_name = "", int ID = 0, int num_courses=0);
+	Student(const Student &student_copy);
+	~Student();
+	const Student& operator = (const Student& rhs)
+	{
+		first_name = rhs.first_name;
+		last_name = rhs.last_name;
+		ID = rhs.ID;
+		return *this;
+	}
+	int get_ID() const { return ID; };
+	int get_num_courses() const { return num_courses; };
+	string get_first_name() const { return first_name; };
+	string get_last_name() const { return last_name; };
+	void set_ID(int id) { ID = id; };
+	void set_first_name(string first) {first_name = first;};
+	void set_last_name(string last) { last_name = last; };
+	int add_course(const Course& a, int num_courses);
+	bool remove_course(const Course&);
+	bool is_enrolled(const Course& aCourse) const;
+	bool read_data(istream& in);
+	bool write_data(ostream& out);
+	double tuition_due() const;
+	friend istream& operator >> (istream& in, Student& data);
+	friend ostream& operator << (ostream& out, const Student& data);
+};
+struct Course
+{
+	string department;
+	int number;
+	int credit_hours;
+};
+istream& operator >> (istream& in, Student& data)
+{
+	in >> data.ID >> data.first_name >> data.last_name;
+	return in;
+}
+istream& operator >> (istream& in, Course& data)
+{
+	in >> data.department >> data.number >> data.credit_hours;
+	return in;
+}
+ostream& operator << (ostream& out, const Student& data)
+{
+	out << data.ID << data.first_name << data.last_name;
+	return out;
+}
+ostream& operator << (ostream& out, const Course& data)
+{
+	out << data.department << " " << data.number << " " << data.credit_hours << endl;
+	return out;
+}
+
+int Student::add_course(const Course& a, int num_courses)
+{
+	int courses = 3;
+	p_courses = new Course[courses];//here we are assuming our drop out rate is very high
+	if (num_courses == courses)
+	{
+		Course *r;
+		courses = courses * 2 + 1;
+		r = new Course[courses];
+		for (int j = 0; j < num_courses; j++)
+		{
+			r[j] = p_courses[j];
+		}
+		if (p_courses != nullptr)
+			delete[] p_courses;
+		p_courses = r;
+	}
+	p_courses[num_courses] = a;
+	num_courses++;
+	return num_courses;
+}
+bool Student::remove_course(const Course& a)
+{}
+bool Student::is_enrolled(const Course& aCourse) const
+{}
+bool Student::read_data(istream& in)
+{
+	in >> ID >> first_name >> last_name >> num_courses;
+	for (int i = 0; i < num_courses; i++)
+	{
+		add_course
+	}
+	if (first_name.isdigit() || last_name.isdigit())
+		return false;
+	else
+		return true;
+
+}
+bool Student::write_data(ostream& out)
+{
+	out << ID << " " << first_name << " " << last_name << " " << num_courses;
+
+}
+double Student::tuition_due() const
+{
+	float total=0;
+	float grand_total = 10;//library fee
+	float pastry_fund = 0;
+	total += 476 * a.credit_hours;
+	total += 0.75 * a.credit_hours;
+	if (a.department == "CS" || a.department == "IT" || a.department == "ECE" || a.department == "CE" || a.department == "ME")
+		total += 15 * a.credit_hours;//lab expenses
+	grand_total += total;
+	pastry_fund = grand_total*0.0025;
+	grand_total += pastry_fund;
+	return total;
+}
+
+#endif
